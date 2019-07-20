85252ee903cdce923515038c3614018be18e8ea2
unknown <wipeautcrafter@gmail.com>
2019-07-20 14:06:55 +0200

    minder aids

--- app/script.js
+++ app/script.js
@@ -5,33 +5,24 @@ socket.on("update", function(data) {
   $(".subtitle").html(data.subtitle);
   $(".info").html(data.info);
 
-  const diff = data.streams - $(".content").children().length;
+  for(let i = 0; i < data.streams; i++) {
+    let url = "http://localhost:8000/live/"+i+".flv";
+    console.log(url);
+    const el = $('<video controls autoplay></video>').appendTo(".content")[0];
 
-  if(diff > 0) {
-    for(let i = 0; i < diff; i++) {
-      let url = "http://localhost:8000/live/"+i+".flv";
-      console.log(url);
-      const el = $('<video controls autoplay></video>').appendTo(".content")[0];
+    let player = flvjs.createPlayer({
+        type: 'flv',
+        url: url
+    },
+    {
+        enableWorker: false,
+        lazyLoadMaxDuration: 3 * 60,
+        seekType: 'range',
+    });
 
-      let player = flvjs.createPlayer({
-          type: 'flv',
-          url: url
-      },
-      {
-          enableWorker: false,
-          lazyLoadMaxDuration: 3 * 60,
-          seekType: 'range',
-      });
-
-      player.attachMediaElement(el);
-      player.load();
-      player.play();
-    }
-  } else if(diff < 0) {
-    const children = $(".content").children().length;
-    for(let i = children; i > children + diff; i--) {
-      $(".content").children().eq(i-1).remove();
-    }
+    player.attachMediaElement(el);
+    player.load();
+    player.play();
   }
 });
 
