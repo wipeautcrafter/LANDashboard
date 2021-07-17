69f5a3af44eb85b6fe672b0c80aa0f914cdf7ff4
wipeautcrafter <wipeautcrafter@gmail.com>
2021-07-17 22:53:37 +0200

    fixed indexed streams

--- app/script.js
+++ app/script.js
@@ -1,33 +1,36 @@
 const socket = io("http://localhost:3000");
+const clients = {};
 
 socket.on("update", function(data) {
   $(".title").html(data.title);
   $(".subtitle").html(data.subtitle);
   $(".info").html(data.info);
-
-  $(".content").html("");
-
-  for(let i = 0; i < data.streams; i++) {
-    let url = "http://localhost:8000/live/"+i+".flv";
-    const el = $('<video controls autoplay></video>').appendTo(".content")[0];
-
-    let player = flvjs.createPlayer({
-        type: 'flv',
-        isLive: true,
-        url: url
-    },
-    {
-        enableWorker: false,
-        lazyLoadMaxDuration: 3 * 60,
-        seekType: 'range',
-    });
-
-    player.attachMediaElement(el);
-    player.load();
-    player.play();
-  }
 });
 
-$(document).ready(function() {
+socket.on("join", function(id) {
+  const url = "http://localhost:8000/live/"+id+".flv";
+  const el = $('<video controls autoplay></video>').appendTo(".content")[0];
 
+  let player = flvjs.createPlayer({
+    url: url,
+    type: "flv",
+    isLive: true
+  },
+  {
+    autoCleanupSourceBuffer: true
+  });
+
+  player.attachMediaElement(el);
+  player.load();
+  player.play();
+
+  clients[id] = {
+    element: el,
+    player: player
+  };
+});
+
+socket.on("leave", function(id) {
+  clients[id].player.destroy();
+  clients[id].element.remove();
 });
--- index.js
+++ index.js
@@ -1,26 +1,9 @@
+const NodeMediaServer = require('node-media-server');
 const {app, BrowserWindow} = require("electron");
 const ip = require("ip");
 const path = require("path");
 
-const NodeMediaServer = require('node-media-server');
-
-const nms = new NodeMediaServer({
-  rtmp: {
-    port: 1935,
-    chunk_size: 60000,
-    gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
-  },
-  http: {
-    port: 8000,
-    allow_origin: '*'
-  }
-});
-
-const express = require("express")();
-const http = require("http").Server(express);
-const io = require("socket.io")(http);
+// CONFIG
 
 const config = {
   title: '<i class="fas fa-gamepad"></i> LAN Dashboard',
@@ -29,14 +12,68 @@ const config = {
   streams: 4
 };
 
-let appConnected = false;
+// MEDIA SERVER
+
+const nms = new NodeMediaServer({
+  logType: 0,
+  rtmp: {
+    port: 1935,
+    chunk_size: 512000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+});
+
+const clients = {};
+
+nms.on("prePublish", function(id, path) {
+  const key = path.split("/").slice(-1)[0];
+  
+  if(key >= 0 && key < config.streams) {
+    console.log(`Stream ${key} started.`);
+
+    io.emit("join", key);
+    clients[key] = id;
+  } else {
+    console.log(`Stream ${key} rejected: above stream limit!`);
+
+    const session = nms.getSession(id);
+    session.reject();
+  }
+});
+
+nms.on("donePublish", function(id, path) {
+  const key = path.split("/").slice(-1)[0];
+  
+  if(key >= 0 && key < config.streams) {
+    console.log(`Stream ${key} stopped.`);
+
+    io.emit("leave", key);
+    delete clients[key];
+  }
+});
+
+// EXPRESS FILE SERVICE
+
+const express = require("express")();
+const http = require("http").Server(express);
+const io = require("socket.io")(http);
+
 
 express.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "remote/index.html"));
 });
+
 express.get("/script.js", function(req, res) {
   res.sendFile(path.join(__dirname, "remote/script.js"));
 });
+
+// SOCKET IO
 
 io.on("connection", function(socket) {
   socket.emit("update", config);
@@ -49,6 +86,18 @@ io.on("connection", function(socket) {
       config.streams = parseInt(conf.streams);
 
       io.emit("update", config);
+
+      Object.keys(clients).forEach(key => {
+        if(key >= config.streams) {
+          const session = nms.getSession(clients[key]);
+          session.stop();
+
+          io.emit("leave", key);
+          delete clients[key];
+
+          console.log(`Stream ${key} closed: above stream limit!`);
+        }
+      });
     } catch(err) {
       console.log("invalid request buddy");
     }
@@ -56,6 +105,8 @@ io.on("connection", function(socket) {
 });
 
 http.listen(3000);
+
+// ELECTRON
 
 app.on("ready", function() {
   const win = new BrowserWindow({
@@ -70,6 +121,10 @@ app.on("ready", function() {
   win.setMenu(null);
   win.maximize();
   win.setFullScreen(true);
+});
+
+app.on("window-all-closed", function() {
+  app.exit();
 });
 
 nms.run();
